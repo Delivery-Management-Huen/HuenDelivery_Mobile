e9c875a5c28803d5a74e0419d9a75bbe268e4e9f
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-01-19 16:10:10 +0900

    Fix: Delivery Network에 End 요청 추가

--- lib/network/auth_network.dart
+++ lib/network/auth_network.dart
@@ -1,9 +1,9 @@
 import 'package:http/http.dart' as http;
-import 'package:huen_delivery_mobile/config/config.dart';
+import 'package:huen_delivery_mobile/network/base_network.dart';
 
 class AuthNetwork {
   Future<http.Response> login(String id, String pw) async {
-    final res = await http.post('$END_POINT/auth/login/driver', body: {
+    final res = await postNetwork('/auth/login/driver', {
       'id': id,
       'password': pw,
     });
